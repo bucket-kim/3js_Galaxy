44c943834c4315564783d488616c9271227ce938
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-06-21 16:06:01 +0900

    a little more edit

--- src/script.js
+++ src/script.js
@@ -135,12 +135,6 @@ gui
   .step(100)
   .onFinishChange(generateGalaxy);
 gui
-  .add(parameters, "size")
-  .min(0.001)
-  .max(0.1)
-  .step(0.001)
-  .onFinishChange(generateGalaxy);
-gui
   .add(parameters, "radius")
   .min(0.01)
   .max(20)
@@ -149,24 +143,18 @@ gui
 gui
   .add(parameters, "branches")
   .min(2)
-  .max(12)
+  .max(20)
   .step(1)
-  .onFinishChange(generateGalaxy);
-gui
-  .add(parameters, "spin")
-  .min(-5)
-  .max(5)
-  .step(0.001)
   .onFinishChange(generateGalaxy);
 gui
   .add(parameters, "randomness")
   .min(0)
-  .max(1)
+  .max(2)
   .step(0.001)
   .onFinishChange(generateGalaxy);
 gui
   .add(parameters, "randomnessPower")
-  .min(0)
+  .min(1)
   .max(10)
   .step(0.001)
   .onFinishChange(generateGalaxy);
